6998a38a5a6c6cf758ee8167eda4846883026c1c
Ossy <drake@OSSMAN>
2025-09-03 18:08:12 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantMVC.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RestaurantMVC.Controllers
+{
+    public class HomeController(IHttpClientFactory httpClientFactory) : Controller
+    {
+        private readonly HttpClient _httpClient = httpClientFactory.CreateClient("RestaurantAPI");
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync("Menu/GetWholeMenu");
+
+
+            var apiResponse = await response.Content.ReadFromJsonAsync<ApiResponse<List<MenuItem>>>();
+            var menu = apiResponse?.Data;
+            return View(menu);
+        }
+    }
+}
---
+++ Models/MenuItem.cs
@@ -0,0 +1,11 @@
+﻿namespace RestaurantMVC.Models
+{
+    public class MenuItem
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public decimal Price { get; set; } 
+        public bool IsPopular { get; set; } 
+    }
+}
