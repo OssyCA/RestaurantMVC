aa25825e216bdec04de6b0fe79fd5d42abf1005a
Ossy <drake@OSSMAN>
2025-09-12 13:42:18 +0200

    Fixeed extra buttons

---
+++ ViewModels/BookingVM.cs
@@ -0,0 +1,18 @@
+﻿using RestaurantMVC.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantMVC.ViewModels
+{
+    public class BookingVM
+    {
+        public int BookingId { get; set; }
+        public int CustomerId { get; set; }
+        public int RestaurantTableId { get; set; }
+        public DateTime BookedAt { get; set; }
+        public DateTime StartAt { get; set; }
+        public DateTime? ExpireAt { get; set; }
+        public int Amount { get; set; }
+    }
+
+}
