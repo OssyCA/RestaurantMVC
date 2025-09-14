dcb6189bdee1676a52e52fc6f38f1c6a8a97b146
Ossy <drake@OSSMAN>
2025-09-14 17:30:30 +0200

    dds

---
+++ Helper/HtmlHelperExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RestaurantMVC.Helper
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string IsActive(this IHtmlHelper html, string controller)
+        {
+            var currentController = html.ViewContext.RouteData.Values["controller"]?.ToString();
+            return controller == currentController ? "active" : "";
+        }
+    }
+}
